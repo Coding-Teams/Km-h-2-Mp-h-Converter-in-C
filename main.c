cfe783f3ab29f7d81faa6ec3f542a5dee79e79bc
Fab_ <30447649+Fabio-Git-dev@users.noreply.github.com>
2020-11-25 23:05:42 +0100

    Add files via upload
    Thanks to FunkierGroose for the help in improving the code.

--- main.c
+++ main.c
@@ -4,53 +4,114 @@
 
  int main() {  //beginning of the function
 
-
-
-    float mp_h;
-	float km_h = 1.609; //variables
-	float conversion;
+    float conversion;  //variable operands
 	float conversion_reverse;
-	int choise;
-
- do {
 
 
-    printf("km/h2mp/h converter by Fabio - v1.0a \n");
-
-	printf("Insert mp-h number \n");
-	scanf("%f", &mp_h);
-
-	conversion = mp_h * km_h; //operand for conversion
+    int choice;
+	float mp_h;
+	float km_h = 1.609; //variables for choice
 
 
-	printf("Converting to km-h... \n");
-
-    printf("%f\n", conversion); //output operand
-
-    //Reverse Operation.
-
-    printf("Insert km_h number \n");
-	scanf("%f", &km_h);
-
-	conversion_reverse = km_h / mp_h; //operand for conversion_reverse
-
-
-	printf("Converting to mp-h... \n");
+	int decision; //variables for decision
 
 
 
-    printf("%f\n", conversion_reverse); //output operand
-
-    printf("Do you want to continue? 1 (yes) 2(no) \n");
-
-    scanf("%d", &choise);
-
-  } while (choise == 1);
 
 
-      system("PAUSE");
+	printf("km/h2mp/h converter by Fabio - v1.0a \n");
 
-      return 0;
+	do {
+	    printf("Wich unit do you want to convert? 1(Mp/h) 2(km/h) 3(Exit)\n"); //Unit choice.
+	scanf("%d", &choice);
+
+
+
+
+
+switch (choice) { //using switch for multiple cases, linked to the user choise.
+
+  case 1: //mp_h 2 km_H
+
+      do {
+         printf("Insert mp/h number \n");
+	     scanf("%f", &mp_h);
+
+         conversion = mp_h * km_h; //operand for conversion
+
+
+	     printf("Converting to km/h... \n");
+
+         printf("%f\n", conversion); //output operand
+
+         printf("Do you want to continue? 1(Yes) 2(No) \n");
+         scanf("%d", &decision);
+
+      } while (decision == 1);
+
+
+         break; //pause instruction.
+
+
+
+  case 2: //km_h 2 mp_h
+
+      do {
+         printf("Insert km/h number \n");
+	     scanf("%f", &km_h);
+
+	     conversion = km_h / mp_h; //operand for conversion
+
+
+	     printf("Converting to km/h... \n");
+
+         printf("%f\n", conversion); //output operand
+
+         printf("Do you want to continue? 1(Yes) 2(No) \n");
+         scanf("%d", &decision);
+
+      } while (decision == 1);
+
+	     break;
+
+
+  default:  //any value that it's not recognized inside the switch parameter. Used to exit the program.
+
+           printf("Exiting program...\n");
+
+           system("PAUSE");
+
+           break;
+	}
+
+
+	} while (choice !=3); //has to be different from number 3, or else it will close)
+
+
+
 
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
